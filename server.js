c9d9b02b35dd4f1112fec3d4e56950dc1f9e0369
artraxus <artraxus@gmail.com>
2014-04-26 17:40:22 +0200

    Create the http server with express

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+    res.sendfile('index.html');
+});
+
+app.listen(8080);
+console.log('Server is running...');
